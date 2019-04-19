530380ded3f8d905ac0623c52739974030bca7f9
antihax <antihax@gmail.com>
2019-04-18 19:33:46 -0700

    Invert lat

--- www/mapviewer.js
+++ www/mapviewer.js
@@ -203,7 +203,7 @@ class WorldMap extends React.Component {
 
       _onMouseMove: function (e) {
         var lng =  L.Util.formatNum(scaleLeafletToAtlas(e.latlng.lng), 2);
-        var lat =  L.Util.formatNum(scaleLeafletToAtlas(-e.latlng.lat), 2);
+        var lat =  -L.Util.formatNum(scaleLeafletToAtlas(-e.latlng.lat), 2);
         var value =  lng + this.options.separator + lat;
         var prefixAndValue = this.options.prefix + ' ' + value;
         this._container.innerHTML = prefixAndValue;
